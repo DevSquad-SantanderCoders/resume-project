b66e015772f908ed3c6ee3a41d8c051c58adf1c7
Matheus Holanda <matheusholanda0997@gmail.com>
2023-10-24 21:33:44 -0300

    feat(): including contents component

---
+++ resume-project/src/app/component/contents/contents.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contents',
+  templateUrl: './contents.component.html',
+  styleUrls: ['./contents.component.scss']
+})
+export class ContentsComponent {
+  text: string = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt iusto cumque rem, sed, a magni laboriosam sint unde deserunt dolorum sequi. Molestias quia architecto consequuntur iusto porro laborum cupiditate illo?'
+}
