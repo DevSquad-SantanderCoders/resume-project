097aa2f63496c86d98e3831b761cfd0f2e513634
Juan Salles <juanprazeres@gmail.com>
2023-10-26 17:39:08 -0300

    feat(): implement photo-loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,7 @@ export class AppComponent {
     name: 'calabreso',
     tel: '44444444',
     email: 'calabreso@matchupitchu.com',
-    img: 'https://img.freepik.com/fotos-gratis/retrato-de-uma-jovem-bonita-em-pe-na-parede-cinza_231208-10760.jpg?w=1380&t=st=1698276894~exp=1698277494~hmac=f8e4cd2a0d7faedc2705f2de113b72d8b37cb49509bc47f15bd2bb4190c2fa28',
+    img: '../assets/default-user.png',
   };
   skillsInfo: skillData[] = [
     {
